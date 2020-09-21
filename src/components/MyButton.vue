cb1efd464303d3a25318678b34a06d4dc9c89298
kouts <koutsaftakis@gmail.com>
2020-09-21 16:45:57 +0300

    Added button component

---
+++ src/components/MyButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button :style="{ background, color }">
+    Button
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: ''
+    },
+    background: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  button {
+    background: none;
+    color: black;
+    border: none;
+    border-radius: 5px;
+    padding: 10px 40px;
+    font-size: 16px;
+    cursor: pointer;
+    &:hover {
+      filter:brightness(125%);
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,17 +4,24 @@
       <img alt="Vue logo" src="../assets/logo.png" class="text-center">
     </div>
     <hello-world msg="Welcome to Vue.js 3 starter template" />
+    <my-button
+      color="white"
+      background="darkslateblue"
+      :disabled="false"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import MyButton from '@/components/MyButton.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    MyButton
   }
 };
 </script>
