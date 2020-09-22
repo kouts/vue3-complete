40dbf2fd1889004dae8e6060f5a779aaa42087cd
Giannis Koutsaftakis <kouts@192.168.91.134>
2020-09-22 17:28:49 +0300

    Added card styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="container">
-    <h1>Home</h1>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="card">
+          <div class="card-header">
+            Featured
+          </div>
+          <div class="card-body">
+            Content
+          </div>
+          <div class="card-footer">
+            Card footer
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
