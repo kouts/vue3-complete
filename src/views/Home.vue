d2c9a1693bb8a97d925958eb99a480980291866c
Giannis Koutsaftakis <kouts@192.168.91.134>
2020-09-22 21:12:38 +0300

    Added data fetching

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,5 +20,22 @@
 
 <script>
 export default {
+  setup() {
+    const api = 'https://pokeapi.co/api/v2/pokemon';
+    const fetchData = async() => {
+      const response = await window.fetch(`${api}/1`);
+      const data = await response.json();
+      const pokemon = {
+        name: data.name,
+        sprite: data.sprites.other['official-artwork'],
+        types: data.types.map(o => ({ name: o.type.name }))
+      };
+      console.log(pokemon);
+    };
+    fetchData();
+    return {
+      //
+    };
+  }
 };
 </script>
