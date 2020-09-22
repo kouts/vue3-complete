9eb365c93fc81aa0e0c8a3490f2dc6329332557e
Giannis Koutsaftakis <kouts@192.168.91.134>
2020-09-23 00:05:29 +0300

    Added selected card feature

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="container">
+    <h1 class="mb-4">Pokémons</h1>
     <div class="form-row mb-4">
       <div v-for="starter in starters" :key="starter.name" class="col-sm-2">
-        <card-pokemon @click="fetchEvolutions(starter.id)">
+        <card-pokemon :class="[selectedId !== starter.id && 'inactive']" @click="selectPokemon(starter.id)">
           <template #title>
-            {{ starter.name }}
+            {{ starter.name }} #{{ starter.id }}
           </template>
           <template #content>
             <img class="card-img-top" loading="lazy" :src="starter.sprite" :alt="starter.name" />
@@ -19,9 +20,9 @@
     </div>
     <div v-if="evolutions.length" class="form-row mb-4">
       <div v-for="creature in evolutions" :key="creature.name" class="col-sm-2">
-        <card-pokemon>
+        <card-pokemon class="bg-light">
           <template #title>
-            {{ creature.name }}
+            {{ creature.name }} #{{ creature.id }}
           </template>
           <template #content>
             <img class="card-img-top" loading="lazy" :src="creature.sprite" :alt="creature.name" />
@@ -50,6 +51,7 @@ export default {
     const startersIds = [1, 4, 7];
     const starters = ref([]);
     const evolutions = ref([]);
+    const selectedId = ref(0);
 
     const fetchPokemon = async(id) => {
       const response = await window.fetch(`${api}/${id}`);
@@ -61,12 +63,12 @@ export default {
         types: data.types.map(o => o.type.name)
       };
     };
-
     const fetchPokemons = async(ids) => await Promise.all(ids.map(id => fetchPokemon(id)));
+    const fetchEvolutions = async(id) => await fetchPokemons([id + 1, id + 2]);
 
-    const fetchEvolutions = async(id) => {
-      const evolutionIds = [id + 1, id + 2];
-      evolutions.value = await fetchPokemons(evolutionIds);
+    const selectPokemon = async(id) => {
+      evolutions.value = await fetchEvolutions(id);
+      selectedId.value = id;
     };
 
     (async() => {
@@ -77,8 +79,19 @@ export default {
     return {
       starters,
       evolutions,
-      fetchEvolutions
+      selectPokemon,
+      selectedId
     };
   }
 };
 </script>
+
+<style lang="scss" scoped>
+  .inactive {
+    cursor: pointer;
+    opacity: 0.5;
+    &:hover {
+      opacity: 0.7;
+    }
+  }
+</style>
